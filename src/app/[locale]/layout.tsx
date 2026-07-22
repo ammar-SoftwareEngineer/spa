eaf093348a3cd867e7c6a7226f9d51eb27c9c685
ammar-SoftwareEngineer <ammartolba89@gmail.com>
2026-07-22 16:21:53 +0300

    update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,14 +42,14 @@ export async function generateMetadata({
     title: t("title.default"),
     description: t("description.default"),
     icons: {
-      icon: [{ url: "public/img/logo.png", type: "image/png" }],
-      shortcut: "public/img/logo.png",
-      apple: "public/img/logo.png",
+      icon: [{ url: "/img/logo.png", type: "image/png" }],
+      shortcut: "/img/logo.png",
+      apple: "/img/logo.png",
     },
     openGraph: {
       images: [
         {
-          url: "public/img/logo.png",
+          url: "/img/logo.png",
           width: 1200,
           height: 630,
           alt: "S&PA Logo",
